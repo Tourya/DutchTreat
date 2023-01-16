e56fb52c02e444b18919b7b5e9c913fee8ed44b9
Tourya <tourya@abv.bg>
2023-01-16 15:23:58 +0200

    AccountController, LoginView and LoginViewModel created

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "App");
+            }
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+            return View();
+        }
+    }
+}
