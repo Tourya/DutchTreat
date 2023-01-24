ef6ef2707bc7b6b7f4fc430fd3718c1090555945
Tourya <tourya@abv.bg>
2023-01-24 16:27:25 +0200

    Calculated field made for subtotal

--- client/src/app/services/store.service.ts
+++ client/src/app/services/store.service.ts
@@ -23,7 +23,7 @@ export class Store {
     addToOrder(product: Product) {
 
         let item: OrderItem | undefined;
-        item = this.order.items.find(o => o.productArtId === product.artId)
+        item = this.order.items.find(o => o.productId === product.id)
 
         if (item) {
             item.quantity++;
--- client/src/app/shared/Order.ts
+++ client/src/app/shared/Order.ts
@@ -15,4 +15,11 @@ export class Order {
     orderDate: Date = new Date();
     orderNumber!: string;
     items: OrderItem[] = [];
+
+    get subtotal(): number {
+
+        const result = this.items.reduce((tot, val) =>  tot + (val.unitPrice * val.quantity), 0);
+
+        return result;
+    }
 }
