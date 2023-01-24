6a67ad212b85241bc763e8ea0e291923df96654c
Tourya <tourya@abv.bg>
2023-01-24 16:11:58 +0200

    Building the Cart with Angular

--- client/src/app/services/store.service.ts
+++ client/src/app/services/store.service.ts
@@ -1,6 +1,7 @@
 ﻿import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { map, Observable } from "rxjs";
+import { Order, OrderItem } from "../shared/Order";
 import { Product } from "../shared/Product";
 
 @Injectable()
@@ -10,11 +11,35 @@ export class Store {
     }
 
     public products: Product[] = [];
+    public order: Order = new Order();
 
     loadProducts(): Observable<void> {
         return this.http.get<[]>("/api/products").pipe(map(data => {
             this.products = data;
             return;
         }));
+    }
+
+    addToOrder(product: Product) {
+
+        let item: OrderItem | undefined;
+        item = this.order.items.find(o => o.productArtId === product.artId)
+
+        if (item) {
+            item.quantity++;
+        } else {
+            item = new OrderItem();
+            item.productId = product.id;
+            item.productTitle = product.title;
+            item.productArtId = product.artId;
+            item.productArtist = product.artist;
+            item.productCategory = product.category;
+            item.productSize = product.size;
+            item.unitPrice = product.price;
+            item.quantity = 1;
+
+            this.order.items.push(item);
+        }
+
     }
 }
